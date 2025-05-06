dbb4d5373257d6d8e8d38f1bfa32a1e70efe66e8
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-05-06 09:23:58 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,18 +1,75 @@
+import random
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    frutas = ["pera", "banana", "melancia", "laranja", "abacaxi"]
+    fruta_secreta = frutas[random.randint(0, 3)]
+    fruta_espelho = []
 
-	enforcou = False
-	acertou = False
+    for letter in fruta_secreta:
+        fruta_espelho.append("_")
+        
+    enforcou = False
+    acertou = False
+    vidas = 6
+    while(vidas>0 and acertou == False):
+        chute = input("Qual seu próximo CHUTE?")
+        atualizou = False
+        for x in range(len(fruta_secreta)):
+            if(chute == fruta_secreta[x]):
+                  fruta_espelho[x] = chute
+                  atualizou = True
+        if(not atualizou): vidas-=1
+                  
+        resultado = ''.join(fruta_espelho)
+        print("______")
+        print("|    |")
+        print("|     ")
+        print("|      ")
+        print("|      ")
+        if(vidas<=5): 
+            print("______")
+            print("|    |")
+            print("|    O")
+            print("|      ")
+            print("|      ")
 
-	while(not acertou and not enforcou):
-	    print("Jogando...")
-
-    print("Fim do jogo")
-
+        if(vidas<=4): 
+            print("______")
+            print("|    |")
+            print("|    O")
+            print("|    | ")
+            print("|      ")
+        if(vidas<=3): 
+            print("______")
+            print("|    |")
+            print("|    O")
+            print("|    |\\")
+            print("|      ")
+        if(vidas<=2): 
+            print("______")
+            print("|    |")
+            print("|    O")
+            print("|   /|\\")
+            print("|      ")
+        if(vidas<=1): 
+            print("______")
+            print("|    |")
+            print("|    O")
+            print("|   /|\\")
+            print("|     \\")
+        if(vidas<=0): 
+            print("______")
+            print("|    |")
+            print("|    O")
+            print("|   /|\\")
+            print("|   / \\")
+        print(resultado)  # Saída: _a_a_a 
+        if(resultado == fruta_secreta): 
+            acertou = True
+            print("Parabéns, você acertou!")
 
 if(__name__ == "__main__"):
     jogar()
