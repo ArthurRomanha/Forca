251f27d53b260e3a37046787e222ec093cb09093
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-05-27 08:36:37 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -9,16 +9,56 @@ def jogar():
     print("|      ")
     print("|      ")
 
+    fruta_secreta = randomWord()
+    loopGame(fruta_secreta)
+    
+def randomWord():
     arquivo = open("./palavras.txt")
     frutas = []
-    chutes = []
     for linha in arquivo:
         linha=linha.strip()
         frutas.append(linha)
     fruta_secreta = frutas[random.randint(0, (len(frutas)-1))]
+    return fruta_secreta
+def desenhaBoneco(vidas, resultado):
+    if(vidas==5): 
+        print("______")
+        print("|    |")
+        print("|    O")
+        print("|      ")
+        print("|      ")
+    elif(vidas==4): 
+        print("______")
+        print("|    |")
+        print("|    O")
+        print("|    | ")
+        print("|      ")
+    elif(vidas==3): 
+        print("______")
+        print("|    |")
+        print("|    O")
+        print("|    |\\")
+        print("|      ")
+    elif(vidas==2): 
+        print("______")
+        print("|    |")
+        print("|    O")
+        print("|   /|\\")
+        print("|      ")
+    elif(vidas==1): 
+        print("______")
+        print("|    |")
+        print("|    O")
+        print("|   /|\\")
+        print("|     \\")
+    elif(vidas==0): 
+        print("______")
+        print("|    |")
+        print("|    O")
+        print("|   /|\\")
+        print("|   / \\")
+def loopGame(fruta_secreta):
     fruta_espelho = []
-    print(fruta_secreta)
-
     for letter in fruta_secreta:
         fruta_espelho.append("_")
     acertou = False
@@ -33,47 +73,13 @@ def jogar():
         if(not atualizou): vidas-=1
                   
         resultado = ''.join(fruta_espelho)
+        desenhaBoneco(vidas, resultado)
+        verificaAcerto(resultado, fruta_secreta)
         
-        if(vidas==5): 
-            print("______")
-            print("|    |")
-            print("|    O")
-            print("|      ")
-            print("|      ")
-        elif(vidas==4): 
-            print("______")
-            print("|    |")
-            print("|    O")
-            print("|    | ")
-            print("|      ")
-        elif(vidas==3): 
-            print("______")
-            print("|    |")
-            print("|    O")
-            print("|    |\\")
-            print("|      ")
-        elif(vidas==2): 
-            print("______")
-            print("|    |")
-            print("|    O")
-            print("|   /|\\")
-            print("|      ")
-        elif(vidas==1): 
-            print("______")
-            print("|    |")
-            print("|    O")
-            print("|   /|\\")
-            print("|     \\")
-        elif(vidas==0): 
-            print("______")
-            print("|    |")
-            print("|    O")
-            print("|   /|\\")
-            print("|   / \\")
-        print(resultado)
-        if(resultado == fruta_secreta): 
-            acertou = True
-            print("Parabéns, você acertou!")
+def verificaAcerto(resultado, fruta_secreta):
+    if(resultado == fruta_secreta): 
+                acertou = True
+                print("Parabéns, você acertou!")
 
 if(__name__ == "__main__"):
     jogar()
